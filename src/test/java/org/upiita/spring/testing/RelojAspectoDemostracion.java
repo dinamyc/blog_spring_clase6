613b6b96a4ae22f41204e656d5943c773b5189dd
dinamyc <j.luis.enriquez77@gmail.com>
2014-07-12 09:47:23 -0500

    prueba repo git

---
+++ src/test/java/org/upiita/spring/testing/RelojAspectoDemostracion.java
@@ -0,0 +1,22 @@
+package org.upiita.spring.testing;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.upiita.spring.dao.PostDAO;
+
+public class RelojAspectoDemostracion {
+
+	private static ApplicationContext contexto;
+
+	private static PostDAO postDAO;
+	
+	public static void main(String args[]) {
+
+		contexto = new ClassPathXmlApplicationContext("dao-context-testing.xml");
+		postDAO = (PostDAO) contexto.getBean("postDAO");
+		
+		postDAO.buscaPorId(1);
+		
+	}
+
+}
